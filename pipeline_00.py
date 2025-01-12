5770165a22997f87768829cea33db5bbebb0d987
capacitor22 <kpzti@outlook.com>
2025-01-12 18:01:20 -0300

    Exemplo ETL salvando no noSQL TinyDB

---
+++ pipeline_00.py
@@ -0,0 +1,41 @@
+import requests
+import time
+from tinydb import TinyDB
+from datetime import datetime
+
+def extract_dados_bitcoin():
+    url = "https://api.coinbase.com/v2/prices/spot"
+    response = requests.get(url)
+    dados = response.json()
+    return dados
+
+def transform_dados_bitcoin(dados):
+    valor = dados["data"]["amount"]
+    criptomoeda = dados["data"]["base"]
+    moeda = dados["data"]["currency"]
+    timestamp = datetime.now().timestamp()
+
+    dados_transformados = {
+        "valor": valor,
+        "criptomoeda": criptomoeda,
+        "moeda": moeda,
+        "timestamp": timestamp
+    }
+
+    return dados_transformados
+
+def salvar_dados_tinydb(dados, db_name="bitcoin.json"):
+    # Apenas como teste pois nao aceita acessos simultâneos
+    db = TinyDB(db_name)
+    db.insert(dados)
+    print("Dados salvos com sucesso!")
+
+
+if __name__ == "__main__":
+    #Extração dos dados
+    while True:
+        dados_json = extract_dados_bitcoin()
+        dados_tratados = transform_dados_bitcoin(dados_json)
+        salvar_dados_tinydb(dados_tratados)
+        time.sleep(15)
+
